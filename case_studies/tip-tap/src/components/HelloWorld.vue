2ba606898c9e12543c15c4a1c9e4699342afb383
muhyasin89 <muhyasin89@gmail.com>
2019-07-26 19:47:57 +0700

    [UPDATE]add case_studies

---
+++ case_studies/tip-tap/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <text-input></text-input>
+    <h3>Text Editor</h3>
+    <text-editor></text-editor>
+    <h3>Text  Basic</h3>
+    <text-basic></text-basic>
+    <h3>Hidding Menu</h3>
+    <text-menu-hidding></text-menu-hidding>
+  </div>
+</template>
+
+<script>
+import RichTextEditor from '@/components/TipTap'
+import RichTextComplete from '@/components/TipTapComplete'
+import RichTextBasic from '@/components/TipTapBasic'
+import HidingMenu from '@/components/TipTapHidingMenu'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    'text-input': RichTextEditor,
+    'text-editor': RichTextComplete,
+    'text-basic': RichTextBasic,
+    'text-menu-hidding': HidingMenu
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
